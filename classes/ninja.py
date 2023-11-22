80c00817c5dc6817fd96f2f086a419aea3560e1c
unity-team-developers <cesarmartins.pro@gmail.com>
2023-11-22 03:40:51 +0000

    Character movment lef & right & attack

--- classes/ninja.py
+++ classes/ninja.py
@@ -1,31 +1,79 @@
 import pygame
 import os
 
+velocidade = 20
+
 class Ninja(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        """ Carrega todos os atributos necessários"""
         super().__init__()
+        self.rect = pygame.Rect(x, y, 32, 32)
+        self.x = x
+        self.y = y
+        self.velX = 0
+        self.velY = 0
         self.animation_frames = []
         self.frame_index = 0
+        self.correndo = False
+        self.direcao = 1  # 1 para direita, -1 para esquerda
+        self.atacando = False  # Adiciona uma variável para controlar o ataque
         self.load_animation()
         self.image = self.animation_frames[self.frame_index]
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect.topleft = (x, y)
         self.elapsed = 0
 
     def load_animation(self):
-        """ Carrega todas as imagens das animações"""
-        animation_folder = "assets/ninja/idle"
+        self.animation_frames = []  # Limpa as animações existentes
+        if self.atacando:
+            animation_folder = "assets/ninja/attack"
+        elif self.correndo:
+            animation_folder = "assets/ninja/run"
+        else:
+            animation_folder = "assets/ninja/idle"
+
         for filename in os.listdir(animation_folder):
             img = pygame.image.load(os.path.join(animation_folder, filename))
             self.animation_frames.append(img)
 
+    def draw(self, win):
+        pass  # Se precisar de uma função para desenhar, adicione aqui
+
     def update(self):
-        """ Atualiza os frames da animação de forma independente do FPS do game"""
-        if self.elapsed == 0 or pygame.time.get_ticks() - self.elapsed > 20: # Regula a velocidade da animação
+        if self.elapsed == 0 or pygame.time.get_ticks() - self.elapsed > 20:
             self.animate()
             self.elapsed = pygame.time.get_ticks()
-        self.image = self.animation_frames[self.frame_index]
+        self.image = pygame.transform.flip(self.animation_frames[self.frame_index], self.direcao == -1, False)
+        self.rect.topleft = (self.x, self.y)
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.velX = -velocidade
+            self.correndo = True
+            self.direcao = -1  # Define a direção para a esquerda
+        elif keys[pygame.K_RIGHT]:
+            self.velX = velocidade
+            self.correndo = True
+            self.direcao = 1  # Define a direção para a direita
+        else:
+            self.velX = 0
+            self.correndo = False
+
+        if keys[pygame.K_UP]:
+            self.velY = -velocidade
+        elif keys[pygame.K_DOWN]:
+            self.velY = velocidade
+        else:
+            self.velY = 0
+
+        if keys[pygame.K_x]:
+            self.atacando = True
+        else:
+            self.atacando = False
+
+        self.x += self.velX
+        self.y += self.velY
+
+        self.load_animation()
 
     def animate(self):
         self.frame_index += 1
--- main.py
+++ main.py
@@ -10,19 +10,23 @@ altura_janela = 1020
 janela = pygame.display.set_mode((largura_janela, altura_janela))
 pygame.display.set_caption("Jogo de Ninja")
 
+x = 500
+y = 500
+
+
 # Crie uma instância do ninja
-ninja = Ninja(largura_janela // 2, altura_janela // 2)
+ninja = Ninja(x, y)
 
 executando = True
 while executando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             executando = False
-
-    # Lógica do jogo (movimento, colisões, etc.)
+        
 
     # Limpe a tela
     janela.fill((0, 0, 0))
+
 
     # Atualize a posição do ninja
     ninja.update()
